a94cf941eb79514899b6e2a2b1bb08641cea66eb
Isaac Besora Vilardaga <isaac.besora@gmail.com>
2018-11-23 19:42:51 +0100

    Adds Baip project header image

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -25,13 +25,12 @@ class MainContent extends Component {
 					<p>Visitors of the exhibition interact with the virtual reproduction in an immersive <b>VR kiosk</b> using a touch-screen and a back-projection display screen with passive stereo. Visitors can simply navigate and zoom-in in different parts of the entrance, or they can get further information just 
 					by touching different hotspots that make 3D information boards appear in front of important components of the façade.</p>
 					<p>I helped with the process of <b>repairing of holes</b> on the mesh resulting from the acquisition process implementing an algorithm to detect them and I was responsible for the implementation of the <b>custom stereo renderer</b> that was used to interactively inspect the mesh.</p>
-					<p>More information about the technical aspects of this project can be found on the following scientific papers:
-						<ul>
-							<li><Paper title="Real-time exploration of the virtual reconstruction of the entrance of the Ripoll Monastery" journal="CEIG 2008 proceedings p. 219-224" url="http://www.lsi.upc.edu/~jmoyes/papers/Portalada.pdf" /></li>
-							<li><Paper title="Portalada: A Virtual Reconstruction of the Entrance of the Ripoll Monastery" journal="Proceedings of 3DPVT08: Fourth International Symposium on 3D Data Processing, Visualization and Transmission" url="http://vcg.isti.cnr.it/Publications/2008/BBCCCDMMRS08/Besora_etal_Portalada08.pdf"/></li>
-							<li><Paper title="Multiscale acquisition and presentation of very large artifacts: The case of portalada" journal="ACM Journal on Computing and Cultural Heritage, Volume 3, Issue 4" url="hhttps://dl.acm.org/citation.cfm?id=1957827"/></li>
-						</ul>
-					</p>
+					<p>More information about the technical aspects of this project can be found on the following scientific papers:</p>
+					<ul>
+						<li><Paper title="Real-time exploration of the virtual reconstruction of the entrance of the Ripoll Monastery" journal="CEIG 2008 proceedings p. 219-224" url="http://www.lsi.upc.edu/~jmoyes/papers/Portalada.pdf" /></li>
+						<li><Paper title="Portalada: A Virtual Reconstruction of the Entrance of the Ripoll Monastery" journal="Proceedings of 3DPVT08: Fourth International Symposium on 3D Data Processing, Visualization and Transmission" url="http://vcg.isti.cnr.it/Publications/2008/BBCCCDMMRS08/Besora_etal_Portalada08.pdf"/></li>
+						<li><Paper title="Multiscale acquisition and presentation of very large artifacts: The case of portalada" journal="ACM Journal on Computing and Cultural Heritage, Volume 3, Issue 4" url="hhttps://dl.acm.org/citation.cfm?id=1957827"/></li>
+					</ul>
 					<div className={ SharedStyles.row }>
 						<div className= { SharedStyles.col1 }>
 							<img src="/files/rendering/portalada2.jpg" alt="Detail of the portalada"></img>
@@ -49,11 +48,10 @@ class MainContent extends Component {
 					<p>The result was a <b>VR kiosk</b> composed of a touch-screen and a back-projection display screen with passive stereo. People were able to move between different attractions of the old Tarraco via a map on the touch-screen and once in-place they were able to move and look freely to examine them as if they were there. In addition to 
 					the examination mode there was also a game mode where the user should go to different places, talk to some non-player characters and earn points by answering questions about the Roman empire.</p>
 					<p>I was on the team responsible of the <b>custom stereo renderer</b> development.</p>
-					<p>More information about the technical aspects of this project can be found on the following scientific papers:
-						<ul>
-							<li><Paper title="La realidad virtual en el aprendizaje de historia mediante la interacción con humanoides virtuales" journal="JOREVIR 2009" url="https://www.researchgate.net/publication/267687462_La_realidad_virtual_en_el_aprendizaje_de_historia_mediante_la_interaccion_con_humanoides_virtuales" /></li>
-						</ul>
-					</p>
+					<p>More information about the technical aspects of this project can be found on the following scientific papers:</p>
+					<ul>
+						<li><Paper title="La realidad virtual en el aprendizaje de historia mediante la interacción con humanoides virtuales" journal="JOREVIR 2009" url="https://www.researchgate.net/publication/267687462_La_realidad_virtual_en_el_aprendizaje_de_historia_mediante_la_interaccion_con_humanoides_virtuales" /></li>
+					</ul>
 					<div className={ SharedStyles.row }>
 						<div className= { SharedStyles.col1 }>
 							<img src="/files/rendering/tarraco1.jpg" alt="An NPC character"></img>
