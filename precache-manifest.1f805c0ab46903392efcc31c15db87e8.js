d3f3eaf0cb9c3965e2fb435415196e89dd194202
Isaac Besora Vilardaga <isaac.besora@gmail.com>
2018-12-03 15:43:51 +0100

    Updates

---
+++ precache-manifest.1f805c0ab46903392efcc31c15db87e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "77be1cb0c5f0899a9453",
+    "url": "/portfolio/static/js/main.77be1cb0.chunk.js"
+  },
+  {
+    "revision": "03bb7263520533b67da6",
+    "url": "/portfolio/static/js/1.03bb7263.chunk.js"
+  },
+  {
+    "revision": "77be1cb0c5f0899a9453",
+    "url": "/portfolio/static/css/main.dbc7fff7.chunk.css"
+  },
+  {
+    "revision": "3572a5348f9199f52ad92ec9cc9d28b9",
+    "url": "/portfolio/index.html"
+  }
+];
