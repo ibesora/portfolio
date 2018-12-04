6a879fa6ccec46cc6d3fc7c7a148f2eb1f9719c8
Isaac Besora Vilardaga <isaac.besora@gmail.com>
2018-12-04 20:39:25 +0100

    Updates

---
+++ precache-manifest.e855a0b32d07ce20173697157c156b52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "274d0ac1ac367a67abc9",
+    "url": "/portfolio/static/js/main.274d0ac1.chunk.js"
+  },
+  {
+    "revision": "6ceb03e08db9fa973571",
+    "url": "/portfolio/static/js/1.6ceb03e0.chunk.js"
+  },
+  {
+    "revision": "274d0ac1ac367a67abc9",
+    "url": "/portfolio/static/css/main.6bf6af4d.chunk.css"
+  },
+  {
+    "revision": "3260a07bb7331933c6d2f36d02ad29d1",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.bc00f9177bb3b1d52b309ff3aa956db4.js"
+  "/portfolio/precache-manifest.e855a0b32d07ce20173697157c156b52.js"
 );
 
 workbox.clientsClaim();
